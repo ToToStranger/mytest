47ec57fb53477a1b5519f4856f7c777b58478bec
ToToStranger <59272525+ToToStranger@users.noreply.github.com>
2021-01-18 10:35:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const fs = require ('fs')
+const http = require ('http')
+const url = require ('url')
+
+
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    
+    if(!product.organic) output = output.replace(/{%NOTORGANIC%}/g, 'not-organic')
+return output
+}
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempProducts = fs.readFileSync(`${__dirname}/templates/template-products.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+
+const dataObj = JSON.parse(data)
+
+
+
+const server = http.createServer((req,res) => {
+   
+    const { query, pathname } = url.parse(req.url, true)
+    const pathName = req.url
+
+
+//Overview page
+if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html'})
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+    res.end(output)
+    //Product page
+} else if (pathname === '/product'){
+    const product = dataObj[query.id]
+    res.writeHead(200, { 'Content-type': 'text/html'})
+    const output = replaceTemplate(tempProducts.product)
+    res.end(output)
+
+    //Api page 
+} else if (pathname === '/api'){
+    res.writeHead(200, { 'Content-type': 'application/json'})
+    res.end('data')
+    //Not Found
+} else {
+    res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world'
+    })
+    res.end('<h1>Page not found!</h1>')
+}
+})
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening on 8000');
+})
